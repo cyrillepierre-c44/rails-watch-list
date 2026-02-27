875aab00bd543185e85e7fae07061191a45b878b
PIERRE Cyrille <cyrille.pierre@gmail.com>
2026-02-27 13:01:59 +0100

    modif routes ajout de root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :lists, only: [ :index, :new, :create, :show, :destroy ] do
+    root to: "lists#index"
     resources :bookmarks, only: [ :new, :create, :destroy ]
   end
 end
